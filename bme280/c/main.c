88ce0d6661269888eedeb48074800361835f6fcc
Hiroki Kawakami <40638139+Hiroki-Kawakami@users.noreply.github.com>
2021-08-02 11:14:59 +0900

    fix indent

--- bme280/c/main.c
+++ bme280/c/main.c
@@ -54,35 +54,35 @@ int i2c_write(uint8_t reg_addr, const uint8_t* data, uint16_t length) {
 }
 
 void get_calib_param() {
-	uint8_t calib[32] = {};
+    uint8_t calib[32] = {};
     int i;
-	
+    
     i2c_read(0x88, calib, 24);
     i2c_read(0xA1, &calib[24], 1);
     i2c_read(0xE1, &calib[25], 7);
 
-	digT[0] = ((calib[1] << 8) | calib[0]);
-	digT[1] = ((calib[3] << 8) | calib[2]);
-	digT[2] = ((calib[5] << 8) | calib[4]);
-	digP[0] = ((calib[7] << 8) | calib[6]);
-	digP[1] = ((calib[9] << 8) | calib[8]);
-	digP[2] = ((calib[11]<< 8) | calib[10]);
-	digP[3] = ((calib[13]<< 8) | calib[12]);
-	digP[4] = ((calib[15]<< 8) | calib[14]);
-	digP[5] = ((calib[17]<< 8) | calib[16]);
-	digP[6] = ((calib[19]<< 8) | calib[18]);
-	digP[7] = ((calib[21]<< 8) | calib[20]);
-	digP[8] = ((calib[23]<< 8) | calib[22]);
-	digH[0] = ( calib[24] );
-	digH[1] = ((calib[26]<< 8) | calib[25]);
-	digH[2] = ( calib[27] );
-	digH[3] = ((calib[28]<< 4) | (0x0F & calib[29]));
-	digH[4] = ((calib[30]<< 4) | ((calib[29] >> 4) & 0x0F));
-	digH[5] = ( calib[31] );
-	
-	for (i = 1; i < 2; i++) if (digT[i] & 0x8000) digT[i] = (-digT[i] ^ 0xFFFF) + 1;
-	for (i = 1; i < 8; i++) if (digP[i] & 0x8000) digP[i] = (-digP[i] ^ 0xFFFF) + 1;
-	for (i = 0; i < 6; i++) if (digH[i] & 0x8000) digH[i] = (-digH[i] ^ 0xFFFF) + 1;
+    digT[0] = ((calib[1] << 8) | calib[0]);
+    digT[1] = ((calib[3] << 8) | calib[2]);
+    digT[2] = ((calib[5] << 8) | calib[4]);
+    digP[0] = ((calib[7] << 8) | calib[6]);
+    digP[1] = ((calib[9] << 8) | calib[8]);
+    digP[2] = ((calib[11]<< 8) | calib[10]);
+    digP[3] = ((calib[13]<< 8) | calib[12]);
+    digP[4] = ((calib[15]<< 8) | calib[14]);
+    digP[5] = ((calib[17]<< 8) | calib[16]);
+    digP[6] = ((calib[19]<< 8) | calib[18]);
+    digP[7] = ((calib[21]<< 8) | calib[20]);
+    digP[8] = ((calib[23]<< 8) | calib[22]);
+    digH[0] = ( calib[24] );
+    digH[1] = ((calib[26]<< 8) | calib[25]);
+    digH[2] = ( calib[27] );
+    digH[3] = ((calib[28]<< 4) | (0x0F & calib[29]));
+    digH[4] = ((calib[30]<< 4) | ((calib[29] >> 4) & 0x0F));
+    digH[5] = ( calib[31] );
+    
+    for (i = 1; i < 2; i++) if (digT[i] & 0x8000) digT[i] = (-digT[i] ^ 0xFFFF) + 1;
+    for (i = 1; i < 8; i++) if (digP[i] & 0x8000) digP[i] = (-digP[i] ^ 0xFFFF) + 1;
+    for (i = 0; i < 6; i++) if (digH[i] & 0x8000) digH[i] = (-digH[i] ^ 0xFFFF) + 1;
 }
 
 double compensate_P(uint32_t adc_P)  {
@@ -108,18 +108,18 @@ double compensate_P(uint32_t adc_P)  {
 
 double compensate_T(uint32_t adc_T)  {
     double v1 = (adc_T / 16384.0 - digT[0] / 1024.0) * digT[1];
-	double v2 = (adc_T / 131072.0 - digT[0] / 8192.0) * (adc_T / 131072.0 - digT[0] / 8192.0) * digT[2];
-	t_fine = v1 + v2;
-	double temperature = t_fine / 5120.0;
-	return temperature;
+    double v2 = (adc_T / 131072.0 - digT[0] / 8192.0) * (adc_T / 131072.0 - digT[0] / 8192.0) * digT[2];
+    t_fine = v1 + v2;
+    double temperature = t_fine / 5120.0;
+    return temperature;
 }
 
 double compensate_H(uint32_t adc_H)  {
     double var_h = t_fine - 76800.0;
-	if (var_h == 0) return 0;
+    if (var_h == 0) return 0;
     var_h = (adc_H - (digH[3] * 64.0 + digH[4]/16384.0 * var_h)) * (digH[1] / 65536.0 * (1.0 + digH[5] / 67108864.0 * var_h * (1.0 + digH[2] / 67108864.0 * var_h)));
 
-	if (var_h > 100.0) return 100.0;
+    if (var_h > 100.0) return 100.0;
     else if (var_h < 0.0) return 0.0;
     else return var_h;
 }
@@ -128,31 +128,31 @@ void read_data(double *temp, double *pres, double *hum) {
     uint8_t data[8] = {};
     i2c_read(0xF7, data, 8);
 
-	uint32_t pres_raw = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4);
-	uint32_t temp_raw = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4);
-	uint32_t hum_raw  = (data[6] << 8)  |  data[7];
-	
-	*temp = compensate_T(temp_raw);
-	*pres = compensate_P(pres_raw);
-	*hum = compensate_H(hum_raw);
+    uint32_t pres_raw = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4);
+    uint32_t temp_raw = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4);
+    uint32_t hum_raw  = (data[6] << 8)  |  data[7];
+    
+    *temp = compensate_T(temp_raw);
+    *pres = compensate_P(pres_raw);
+    *hum = compensate_H(hum_raw);
 }
 
 void setup() {
-    uint8_t osrs_t = 1;			// Temperature oversampling x 1
-	uint8_t osrs_p = 1;  		// Pressure oversampling x 1
-	uint8_t osrs_h = 1;			// Humidity oversampling x 1
-	uint8_t mode   = 3;			// Normal mode
-	uint8_t t_sb   = 5;			// Tstandby 1000ms
-	uint8_t filter = 0;			// Filter off
-	uint8_t spi3w_en = 0;		// 3-wire SPI Disable
+    uint8_t osrs_t = 1;            // Temperature oversampling x 1
+    uint8_t osrs_p = 1;          // Pressure oversampling x 1
+    uint8_t osrs_h = 1;            // Humidity oversampling x 1
+    uint8_t mode   = 3;            // Normal mode
+    uint8_t t_sb   = 5;            // Tstandby 1000ms
+    uint8_t filter = 0;            // Filter off
+    uint8_t spi3w_en = 0;        // 3-wire SPI Disable
 
-	uint8_t ctrl_meas_reg = (osrs_t << 5) | (osrs_p << 2) | mode;
-	uint8_t config_reg    = (t_sb << 5) | (filter << 2) | spi3w_en;
-	uint8_t ctrl_hum_reg  = osrs_h;
+    uint8_t ctrl_meas_reg = (osrs_t << 5) | (osrs_p << 2) | mode;
+    uint8_t config_reg    = (t_sb << 5) | (filter << 2) | spi3w_en;
+    uint8_t ctrl_hum_reg  = osrs_h;
 
-	i2c_write(0xF2, &ctrl_hum_reg, 1);
-	i2c_write(0xF4, &ctrl_meas_reg, 1);
-	i2c_write(0xF5, &config_reg, 1);
+    i2c_write(0xF2, &ctrl_hum_reg, 1);
+    i2c_write(0xF4, &ctrl_meas_reg, 1);
+    i2c_write(0xF5, &config_reg, 1);
 }
 
 int main() {
